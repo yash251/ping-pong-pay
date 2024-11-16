918bf57bc6b9de3c7cb48d11cb38308cb9f1f00a
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-11-16 23:07:46 +0530

    Added Sepolia to hardhat.config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+const privateKeys = process.env.PRIVATE_KEYS || '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}',
+      accounts: privateKeys.split(','),
+    }
+  }
 };
